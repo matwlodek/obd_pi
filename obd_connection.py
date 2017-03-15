b983cf8e100b6fede16aab7a2956b52a029d551f
matwlodek <matwlodek@wp.pl>
2017-03-15 22:39:36 +0100

    Python upgrade...
    Python 2.7.9 -> 3.4
    Aby korzystać z przycisków i panelu nawigacyjnego, konieczne jest
    używanie barrier - dostępne w Python 3.x

--- obd_connection.py
+++ obd_connection.py
@@ -1,10 +1,11 @@
 import obd
+
 sync_conn = obd.OBD()
 
+
 def serial_port():
-    return obd.scan_serial();
+    return obd.scan_serial()
+
 
 def car_conn_status():
-    return sync_conn.status();
-
-
+    return sync_conn.status()
